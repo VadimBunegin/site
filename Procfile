c4440a947770bc186396c1de09b617375df76177
Bunegin_Dmitriy <dmitriy.bunegin@gmail.com>
2023-03-04 20:16:58 +0500

    init commit

--- Procfile
+++ Procfile
@@ -1,4 +1,1 @@
-web: gunicorn 'os101_django_demo.wsgi'
-# NOTE: the brackets are not included in the actual text, just place the name
-# of your project.
-# the folder name that has a WSGI file within it is your project name
+web: gunicorn django_demo_online.wsgi --log-file -
--- django_demo_online/settings.py
+++ django_demo_online/settings.py
@@ -127,4 +127,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
