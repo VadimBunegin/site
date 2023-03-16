716db630b34bcdc10e4256e399986551a20e1d40
Bunegin_Dmitriy <dmitriy.bunegin@gmail.com>
2023-03-16 16:42:10 +0500

    replace integer field to int in second

--- first/models.py
+++ first/models.py
@@ -5,5 +5,5 @@ from django.db import models
 class History(models.Model):
     date = models.DateTimeField(default=0)
     first = models.IntegerField(default=0)
-    second = models.IntegerField(default=0)
+    second = models.FloatField(default=0)
     result = models.IntegerField(default=0)
